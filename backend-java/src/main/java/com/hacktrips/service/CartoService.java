3147d9de7141e370e6382d1226c96dda70db45cc
lexfaraday <lexfaraday@outlook.com>
2017-01-22 05:34:20 +0100

    batch data

--- backend-java/src/main/java/com/hacktrips/service/CartoService.java
+++ backend-java/src/main/java/com/hacktrips/service/CartoService.java
@@ -1,11 +1,15 @@
 package com.hacktrips.service;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpEntity;
@@ -44,23 +48,23 @@ public class CartoService {
 		// Table creation only once time
 		cartoSQL.setTypeSQL(TypeSQL.CREATE);
 		if (!existsDataset(DATA_SET_01)) {
-			uploadData(cartoSQL.generateDataSet());
+			uploadData(cartoSQL.generateDataSet(), false);
 		}
 		// Inserts
 		cartoSQL.setTypeSQL(TypeSQL.INSERT);
-		uploadData(cartoSQL.generateDataSet());
+		uploadData(cartoSQL.generateDataSet(), false);
 
 		// Create Contamination data set
 		cartoSQL.setTableName(DATA_SET_02);
 		cartoSQL.setSubset(true);
 		cartoSQL.setTypeSQL(TypeSQL.CREATE);
 		if (!existsDataset(DATA_SET_02)) {
-			uploadData(cartoSQL.generateDataSet());
+			uploadData(cartoSQL.generateDataSet(), false);
 		}
 		// Inserts
 		cartoSQL.setTypeSQL(TypeSQL.INSERT);
-		uploadData(cartoSQL.generateDataSet());
-		
+		uploadData(cartoSQL.generateDataSet(), true);
+
 		// Check queue for cartesian objects TODO with queue
 		// if (!cartoSQL.getQueueToTables().isEmpty()) {
 		// cartoSQL.setTypeSQL(TypeSQL.CREATE);
@@ -72,18 +76,18 @@ public class CartoService {
 		return true;
 	}
 
-	public Boolean uploadData(String dataSet) {
-		CartoRS cartoRS = cartoCall(dataSet);
+	public Boolean uploadData(String dataSet, boolean split) {
+		CartoRS cartoRS = cartoCall(dataSet, split);
 		return cartoRS.getError() == null;
 	}
 
 	public Boolean existsDataset(String dataSet) {
 		String select = "SELECT * FROM " + dataSet;
-		CartoRS cartoRS = cartoCall(select);
+		CartoRS cartoRS = cartoCall(select, false);
 		return cartoRS.getError() == null;
 	}
 
-	private CartoRS cartoCall(String dataSet) {
+	private CartoRS cartoCall(String dataSet, boolean split) {
 		CartoRS cartoRS = new CartoRS();
 		try {
 			if (dataSet != null && !dataSet.isEmpty()) {
@@ -92,15 +96,48 @@ public class CartoService {
 				headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 				headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-				MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-				map.add("q", dataSet);
+				try {
+					PrintWriter writer = new PrintWriter("lastDataSet.txt", "UTF-8");
+				
+					
+				
+				if (split) {
 
-				HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map,
-						headers);
-				System.out.println(dataSet);
-				ResponseEntity<String> response = restTemplate.exchange(builder.build().encode().toUri(),
-						HttpMethod.POST, request, String.class);
-				//ResponseEntity<CartoRS> response = restTemplate.exchange(builder.build().encode().toUri(),HttpMethod.POST, request, CartoRS.class);
+					StringBuilder batch = new StringBuilder();
+					String dataSets[] = dataSet.split(";");
+					int count = 0;
+					for (String dt : dataSets) {
+						batch.append(dt + ";");
+						if (++count > 100) {
+							MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+							map.add("q", batch.toString());
+							log.debug(batch.toString());
+							writer.println(batch.toString());
+							HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(
+									map, headers);
+							ResponseEntity<String> response = restTemplate.exchange(builder.build().encode().toUri(),
+									HttpMethod.POST, request, String.class);
+							batch = new StringBuilder();
+							count = 0;
+						}
+					}
+				} else {
+					writer.println(dataSet);
+					// FIIXMEEEEE
+					MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+					map.add("q", dataSet);
+
+					HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(
+							map, headers);
+					ResponseEntity<String> response = restTemplate.exchange(builder.build().encode().toUri(),
+							HttpMethod.POST, request, String.class);
+				}
+				writer.close();
+				} catch (IOException e) {
+				}
+				// ResponseEntity<CartoRS> response =
+				// restTemplate.exchange(builder.build().encode().toUri(),HttpMethod.POST,
+				// request, CartoRS.class);
 			}
 			return cartoRS;// response.getBody();
 		} catch (Exception e) {
